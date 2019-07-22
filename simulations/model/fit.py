92cd0fa7f757a745065b7672f0272a228839a4be
davidbestue <davidsanchezbestue@hotmail.com>
2019-07-22 15:03:53 +0200

    update

--- simulations/model/fit.py
+++ simulations/model/fit.py
@@ -59,3 +59,40 @@ plt.legend()
 plt.show(block=False) 
 
 
+
+y=np.reshape(rate, (512)) 
+X=np.reshape(np.arange(0, 512), (512,1))
+
+# Visualizing the Polymonial Regression results
+def viz_polymonial():
+    plt.figure()
+    plt.scatter(X, y, color='red')
+    plt.plot(X, pol_reg.predict(poly_reg.fit_transform(X)), color='blue')
+    plt.title('Fit Bump')
+    plt.xlabel('Neuron')
+    plt.ylabel('rate')
+    plt.show(block=False)
+    return
+
+
+# Fitting Polynomial Regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+
+score=[]
+for deg_fir in range(1,10):    
+    poly_reg = PolynomialFeatures(degree=deg_fir)
+    X_poly = poly_reg.fit_transform(X)
+    pol_reg = LinearRegression()
+    pol_reg.fit(X_poly, y)
+    score.append( pol_reg.score(X_poly, y) )
+    viz_polymonial()
+
+
+
+plt.figure()
+plt.plot(np.arange(1,10), score)
+plt.show(block=False)
+
+
+
