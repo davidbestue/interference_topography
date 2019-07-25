e482dbea088924126ed244b34a5056951a567e80
davidbestue <davidsanchezbestue@hotmail.com>
2019-07-25 19:06:45 +0200

    Update model.py

--- simulations/model/model.py
+++ simulations/model/model.py
@@ -17,6 +17,8 @@ import multiprocessing
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 import scipy.signal
+from scipy.optimize import curve_fit 
+
 
 
 def decode_rE(rE, a_ini=0, a_fin=360, N=512):
@@ -249,24 +251,6 @@ def model(totalTime, targ_onset, presentation_period, separation, tauE=9, tauI=4
         plt.show(block=False) 
 
 
-    #return final_bias
-    if n_stims ==2:
-        if len(peaks)==2:
-            pb1, pb2 = peaks
-            bias_b1 = np.degrees(theta[pb1]) - np.degrees(pi - separation) 
-            bias_b2 =  np.degrees(pi + separation) - np.degrees(theta[pb2])
-            final_bias = [bias_b1, bias_b2]
-        elif len(peaks)==1: 
-            pb = peaks[0]
-            bias_b1 = np.degrees(theta[pb]) - np.degrees( pi - separation)
-            bias_b2 = np.degrees(pi + separation) - np.degrees(theta[pb])  ## bias (positive means attraction)
-            final_bias = [bias_b1, bias_b2]   
-        else:
-            final_bias=[0,0]
-            print('Error in simulation')
-    else:
-        final_bias=0
-
     ### Output
     total_sep=np.degrees(2*separation)
     return(np.mean(final_bias), total_sep, GEE, rE, r_squared) #bias_b1, bias_b2)
@@ -279,7 +263,7 @@ def model(totalTime, targ_onset, presentation_period, separation, tauE=9, tauI=4
 #bias_b1, bias_b2, total_sep, GEE = model(totalTime=2000, targ_onset=100,  presentation_period=350, separation=5,tauE=9, tauI=4,  n_stims=2, I0E=0.1, I0I=0.5, GEE=0.022, GEI=0.019, 
 # GIE=0.01 , GII=0.1, sigE=1.5, sigI=1.6, kappa_E=100, kappa_I=5, kappa_stim=20, N=512, plot_connectivity=False, plot_rate=False, plot_hm=True , plot_fit=False) 
 
-bias, total_sep, GEE, rE, error = model(totalTime=2000, targ_onset=100,  presentation_period=350, separation=5,tauE=9, tauI=4,  n_stims=1, I0E=0.1, I0I=0.5, GEE=0.025, GEI=0.019, GIE=0.01 , GII=0.1, sigE=0.5, sigI=1.6, kappa_E=200, kappa_I=20, kappa_stim=75, N=512, plot_connectivity=False, plot_rate=False, plot_hm=True , plot_fit=True) 
+bias, total_sep, GEE, rE, error = model(totalTime=2000, targ_onset=100,  presentation_period=350, separation=5,tauE=9, tauI=4,  n_stims=2, I0E=0.1, I0I=0.5, GEE=0.025, GEI=0.019, GIE=0.01 , GII=0.1, sigE=0.5, sigI=1.6, kappa_E=200, kappa_I=20, kappa_stim=75, N=512, plot_connectivity=False, plot_rate=False, plot_hm=True , plot_fit=True) 
 print(bias, total_sep)
 
 
