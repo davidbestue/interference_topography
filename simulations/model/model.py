79c4b9c8635209015a905801a2e75ffccc0c5cf8
davidbestue <davidsanchezbestue@hotmail.com>
2019-07-26 11:51:54 +0200

    Update model.py

--- simulations/model/model.py
+++ simulations/model/model.py
@@ -219,12 +219,16 @@ def model(totalTime, targ_onset, presentation_period, separation, tauE=9, tauI=4
     if n_stims ==2:
         param, covs = curve_fit(bi_von_misses, X, y)
         ans = (exp( param[1] * cos(X-param[0]))) / (2*pi*scipy.special.i0(param[1])) + (exp( param[3] * cos(X-param[2]))) / (2*pi*scipy.special.i0(param[3])) 
+        
+
         estimated_angle_1=np.degrees(param[0]+pi)  
-        print(estimated_angle_1)
         estimated_angle_2=np.degrees(param[2]+pi)  
-        print(estimated_angle_2)
-        bias_b1 = estimated_angle_1- np.degrees(origin - separation) ### change the error stuff
-        bias_b2 =  np.degrees(origin + separation) - estimated_angle_2 
+        estimated_angles = [estimated_angle_1, estimated_angle_2]
+        estimated_angles.sort()
+        print(estimated_angles)
+
+        bias_b1 = estimated_angles[0] -  np.degrees(origin - separation) ### change the error stuff
+        bias_b2 =  np.degrees(origin + separation) - estimated_angles[1]
         final_bias = [bias_b1, bias_b2]
     elif n_stims ==1:
         param, covs = curve_fit(von_misses, X, y)
