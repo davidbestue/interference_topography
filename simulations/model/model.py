d7c9ba539ff66ea2323ddc87b34820ca0346efb6
davidbestue <davidsanchezbestue@hotmail.com>
2019-07-31 18:18:07 +0200

    Update model.py

--- simulations/model/model.py
+++ simulations/model/model.py
@@ -300,14 +300,14 @@ numcores = multiprocessing.cpu_count() - 1
 
 
 #df_gee_25 = df
-rep_dist = 1
-n_gges= 3 
+rep_dist = 20
+n_gges= 2 
 n_sepa = 9
 
 separations=[5, 7, 10, 12, 13, 14, 15, 17, 20] * rep_dist * n_gges
-Gees = [0.023]*n_sepa*rep_dist + [0.024]*n_sepa*rep_dist + [0.025]*n_sepa*rep_dist
+Gees = [0.023]*n_sepa*rep_dist  + [0.025]*n_sepa*rep_dist
 
-results = Parallel(n_jobs = numcores)(delayed(model)(totalTime=2000, targ_onset=100,  presentation_period=350, separation=sep, tauE=9, tauI=4,  n_stims=2, I0E=0.1, I0I=0.5, GEE=gees, GEI=0.019, GIE=0.01 , GII=0.1, sigE=0.5, sigI=1.6, kappa_E=200, kappa_I=20, kappa_stim=75, N=512, plot_connectivity=False, plot_rate=False, plot_hm=False , plot_fit=False)  for seps, gees in zip(separations, Gees)) 
+results = Parallel(n_jobs = numcores)(delayed(model)(totalTime=2000, targ_onset=100,  presentation_period=350, separation=sep, tauE=9, tauI=4,  n_stims=2, I0E=0.1, I0I=0.5, GEE=gees, GEI=0.019, GIE=0.01 , GII=0.1, sigE=0.5, sigI=1.6, kappa_E=200, kappa_I=20, kappa_stim=75, N=512, plot_connectivity=False, plot_rate=False, plot_hm=False , plot_fit=False)  for sep, gees in zip(separations, Gees)) 
 
 
 
@@ -317,7 +317,6 @@ geess = [results[i][2] for i in range(len(results))]
 succs = [results[i][5] for i in range(len(results))]   
 
 df=pd.DataFrame({'bias':biases, 'separation':separationts, 'GEE':geess, 'success':succs })
-
 
 
 ###Plot Almeida et al
