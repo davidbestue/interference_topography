941af56bb9f80972da5dbe299f7604b9e47aac53
davidbestue <davidsanchezbestue@hotmail.com>
2019-08-02 15:57:56 +0200

    Update model_radial.py

--- simulations/model/model_radial.py
+++ simulations/model/model_radial.py
@@ -82,7 +82,7 @@ def viz_polymonial(X, y, poly_reg, pol_reg):
 # model(totalTime=2000, targ_onset=100,  presentation_period=100, separation=2) 
 
 
-def model(totalTime, targ_onset, presentation_period, positions, tauE=9, tauI=4,  n_stims=2, I0E=0.1, I0I=0.5, GEE=0.022, GEI=0.019, 
+def model(totalTime, targ_onset, presentation_period, separation, tauE=9, tauI=4,  n_stims=2, I0E=0.1, I0I=0.5, GEE=0.022, GEI=0.019, 
  GIE=0.01 , GII=0.1, sigE=0.5, sigI=1.6, kappa_E=100, kappa_I=1.75, kappa_stim=100, N=512, plot_connectivity=False, plot_rate=False, plot_hm=True , plot_fit=True):
     #
     st_sim =time.time()
