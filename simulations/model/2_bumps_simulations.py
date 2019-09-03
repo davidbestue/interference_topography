ba69b1fe721b3c146649cf1b32424e078f862b37
davidbestue <davidsanchezbestue@hotmail.com>
2019-09-03 17:56:16 +0200

    Update 2_bumps_simulations.py

--- simulations/model/2_bumps_simulations.py
+++ simulations/model/2_bumps_simulations.py
@@ -58,7 +58,7 @@ import multiprocessing
 numcores = multiprocessing.cpu_count() - 2
 
 kappa_e_test = [100, 200] 
-kappa_i_test = [12,  20] 
+kappa_i_test = [10,  20] 
 rep_dist = 50
 n_kappas= len(kappa_e_test)
 
@@ -71,7 +71,7 @@ for idx, k in enumerate(kappa_e_test):
 
 
 results = Parallel(n_jobs = numcores)(delayed(model)(totalTime=2000, targ_onset=100,  presentation_period=350, separation=0, tauE=9, tauI=4,  n_stims=1, I0E=0.1, I0I=0.5,
- GEE=0.025, GEI=0.019, GIE=0.01 , GII=0.1, sigE=1.2, sigI=1.6, kappa_E=kape, kappa_I=kapi, kappa_stim=75, N=512, plot_connectivity=False, plot_rate=False, plot_hm=False , plot_fit=False)  for kape, kapi in zip( kappas_e, kappas_i)) 
+ GEE=0.025, GEI=0.019, GIE=0.01 , GII=0.1, sigE=1.4, sigI=1.6, kappa_E=kape, kappa_I=kapi, kappa_stim=75, N=512, plot_connectivity=False, plot_rate=False, plot_hm=False , plot_fit=False)  for kape, kapi in zip( kappas_e, kappas_i)) 
 
 biases = [results[i][0] for i in range(len(results))]
 separationts = [results[i][1] for i in range(len(results))]   
@@ -86,7 +86,7 @@ df.to_excel('single_item_drift_eccentricity_ke_ki.xlsx')
 
 df = df.loc[df['success']==True] 
 plt.figure(figsize=(8,6))
-linares_plot( x="kappas_E", y="bias", order=[100, 150, 200],  palette='viridis', alpha=0.4, point_size=5, df=df) 
+linares_plot( x="kappas_E", y="bias", order=[100, 200],  palette='viridis', alpha=0.4, point_size=5, df=df) 
 plt.title('Drift with eccentricity separation', fontsize=15) #condition title
 plt.gca().spines['right'].set_visible(False) #no right axis
 plt.gca().spines['top'].set_visible(False) #no  top axis
