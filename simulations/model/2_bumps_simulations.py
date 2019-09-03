78f2b812ff98064b995082d56473242ecea72121
davidbestue <davidsanchezbestue@hotmail.com>
2019-09-03 15:06:44 +0200

    Update 2_bumps_simulations.py

--- simulations/model/2_bumps_simulations.py
+++ simulations/model/2_bumps_simulations.py
@@ -16,6 +16,8 @@ n_sepa = len(distances_test)
 separations= distances_test * rep_dist * n_kappas
 
 kappas_e=[]
+kappas_i=[]
+
 for idx, k in enumerate(kappa_e_test):
     kappas_e = kappas_e + [k]*n_sepa*rep_dist
     kappas_i = kappas_i + [kappa_i_test[idx]]*n_sepa*rep_dist
@@ -39,3 +41,18 @@ df=pd.DataFrame({'bias':biases, 'separation':separationts, 'kappas_E':kappas__e,
 #     presentation_period=350, separation=13, tauE=9, tauI=4,  n_stims=1, I0E=0.1, I0I=0.5,
 #     GEE=0.025, GEI=0.019, GIE=0.01 , GII=0.1, sigE=0.8, sigI=1.6, kappa_E=200, kappa_I=20, kappa_stim=75, N=512,
 #     plot_connectivity=True, plot_rate=False, plot_hm=True , plot_fit=True)
+
+
+df = df.loc[df['success']==True] 
+
+plt.figure(figsize=(8,6))
+g = sns.lineplot( x="separation", y="bias", hue='kappas_E', ci=95 , hue_order=[100,200], palette='viridis', 
+                 data=df, legend=False) 
+plt.plot([0, max(df['separation'])], [0,0], 'k--') 
+plt.title('Bias with separation', fontsize=15) #condition title
+plt.gca().spines['right'].set_visible(False) #no right axis
+plt.gca().spines['top'].set_visible(False) #no  top axis
+plt.gca().get_xaxis().tick_bottom()
+plt.gca().get_yaxis().tick_left()
+plt.legend(title='kappaE', loc='upper right', labels=['100', '200'])
+plt.show(block=False)
