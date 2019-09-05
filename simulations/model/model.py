daacb54a70eab1945f943d228b9ef4161272d6f3
davidbestue <davidsanchezbestue@hotmail.com>
2019-09-05 18:20:24 +0200

    Update model.py

--- simulations/model/model.py
+++ simulations/model/model.py
@@ -234,7 +234,7 @@ def model(totalTime, targ_onset, presentation_period, separation, tauE=9, tauI=4
         number_of_bumps = len(scipy.signal.find_peaks(r, 2)[0]) 
         peaks_list.append(number_of_bumps)
 
-    number_of_bumps=min(peaks_list)
+    number_of_bumps=most_common(peaks_list)
 
 
     ### Fit
