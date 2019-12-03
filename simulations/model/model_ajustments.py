55688cdf3890c965cd8e1738717bec6b48816fd3
davidbestue <davidsanchezbestue@hotmail.com>
2019-12-03 12:17:54 +0100

    update

--- simulations/model/model_ajustments.py
+++ simulations/model/model_ajustments.py
@@ -334,8 +334,8 @@ m = model(totalTime=3000, targ_onset=100,  presentation_period=350, angle_separa
     I0E=0.1, I0I=0.5, GEE=0.025, GEI=0.019, GIE=0.01 , GII=0.1, sigE=1, sigI=1.9, kappa_E=200, kappa_I=20, 
     kappa_stim=75, N=512, plot_connectivity=False, plot_rate=False, plot_hm=True , plot_fit=True) 
 
-print(m[5], decode_rE(m[3]))
-print(m[1], m[2])
+# print(m[5], decode_rE(m[3]))
+# print(m[1], m[2])
 
 
 ## Example
