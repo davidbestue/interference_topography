a3ef18fe27a48ea5a458f3b2af72f2904f4d711b
davidbestue <davidsanchezbestue@hotmail.com>
2019-12-05 12:14:42 +0100

    update

--- simulations/model/model_ajustments.py
+++ simulations/model/model_ajustments.py
@@ -20,6 +20,7 @@ import scipy.signal
 from scipy.optimize import curve_fit 
 
 
+## Fucntions to use
 
 
 def decode_rE(rE, a_ini=0, a_fin=360, N=512):
@@ -337,19 +338,17 @@ def model(totalTime, targ_onset, presentation_period, angle_separation, tauE=9, 
 ###
 ####
 
+
+
+# 2 bumps 
+
+
 # m = model(totalTime=3000, targ_onset=100,  presentation_period=350, angle_separation=22, tauE=9, tauI=4,  n_stims=2, 
 #     I0E=0.1, I0I=0.5, GEE=0.025, GEI=0.019, GIE=0.01 , GII=0.1, sigE=1.1, sigI=1.9, kappa_E=225, kappa_I=15, 
 #     kappa_stim=75, N=512, plot_connectivity=False, plot_rate=False, plot_hm=True , plot_fit=False) 
 
 
-from joblib import Parallel, delayed
-import multiprocessing
-
-numcores = multiprocessing.cpu_count() 
-
-
-# 2 bumps 
-
+#numcores = multiprocessing.cpu_count() 
 
 # distances_test = [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 30, 35, 40, 45, 50, 60, 70, 80, 90]
 
@@ -393,6 +392,12 @@ numcores = multiprocessing.cpu_count()
 
 
 # 1 bumps 
+
+
+# m = model(totalTime=3000, targ_onset=100,  presentation_period=350, angle_separation=22, tauE=9, tauI=4,  n_stims=2, 
+#     I0E=0.1, I0I=0.5, GEE=0.025, GEI=0.019, GIE=0.01 , GII=0.1, sigE=1.1, sigI=1.9, kappa_E=225, kappa_I=15, 
+#     kappa_stim=75, N=512, plot_connectivity=False, plot_rate=False, plot_hm=True , plot_fit=False) 
+
 
 
 kappa_e_test = [ 300, 225] 
