dd52cf2bdbd664e38024417c5543eaad24185bb3
davidbestue <davidsanchezbestue@hotmail.com>
2019-12-05 12:56:08 +0100

    update

--- simulations/model/model_ajustments.py
+++ simulations/model/model_ajustments.py
@@ -403,18 +403,22 @@ def model(totalTime, targ_onset, presentation_period, angle_separation, tauE=9, 
 
 # 1 bumps 
 
-m = model(totalTime=3000, targ_onset=100,  presentation_period=350, angle_separation=22, tauE=9, tauI=4,  n_stims=1, 
-    I0E=0.1, I0I=0.5, GEE=0.025, GEI=0.019, GIE=0.01 , GII=0.1, sigE=1.1, sigI=1.9, kappa_E=300, kappa_I=30, 
-    kappa_stim=75, N=512, plot_connectivity=False, plot_rate=False, plot_hm=True , plot_fit=True) 
+# m = model(totalTime=3000, targ_onset=100,  presentation_period=350, angle_separation=22, tauE=9, tauI=4,  n_stims=1, 
+#     I0E=0.1, I0I=0.5, GEE=0.025, GEI=0.019, GIE=0.01 , GII=0.1, sigE=1.1, sigI=1.9, kappa_E=300, kappa_I=30, 
+#     kappa_stim=75, N=512, plot_connectivity=False, plot_rate=False, plot_hm=True , plot_fit=True) 
 
 
 numcores = multiprocessing.cpu_count() 
+print('Numer cores: '+ str(numcores))
+
 
 kappa_e_test = [ 300, 225] 
 kappa_i_test = [ 30, 15]      
 
 rep_dist = 10
 
+kappas_e=[]
+kappas_i=[]
 for idx, k in enumerate(kappa_e_test):
     kappas_e = kappas_e + [k]*rep_dist
     kappas_i = kappas_i + [kappa_i_test[idx]]*rep_dist
