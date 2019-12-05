467fabf032c338606cf8b2b6a3f033d8bd339bb8
davidbestue <davidsanchezbestue@hotmail.com>
2019-12-05 12:54:17 +0100

    update

--- simulations/model/model_ajustments.py
+++ simulations/model/model_ajustments.py
@@ -408,19 +408,33 @@ m = model(totalTime=3000, targ_onset=100,  presentation_period=350, angle_separa
     kappa_stim=75, N=512, plot_connectivity=False, plot_rate=False, plot_hm=True , plot_fit=True) 
 
 
+numcores = multiprocessing.cpu_count() 
 
 kappa_e_test = [ 300, 225] 
 kappa_i_test = [ 30, 15]      
 
-rep_dist = 500
+rep_dist = 10
 
 for idx, k in enumerate(kappa_e_test):
-    kappas_e = kappas_e + [k]*n_sepa*rep_dist
-    kappas_i = kappas_i + [kappa_i_test[idx]]*n_sepa*rep_dist
+    kappas_e = kappas_e + [k]*rep_dist
+    kappas_i = kappas_i + [kappa_i_test[idx]]*rep_dist
 
 
 
 
+results = Parallel(n_jobs = numcores)(delayed(model)(totalTime=2000, targ_onset=100,  presentation_period=350, angle_separation=22, tauE=9, tauI=4,  n_stims=1, I0E=0.1, I0I=0.5,
+ GEE=0.025, GEI=0.019, GIE=0.01 , GII=0.1, sigE=1.1, sigI=1.9, kappa_E=kape, kappa_I=kapi, kappa_stim=75, N=512, plot_connectivity=False, plot_rate=False, plot_hm=False , plot_fit=False)  for kape, kapi in zip(kappas_e, kappas_i)) 
+
+final_biases = [results[i][0] for i in range(len(results))]
+b1 = [results[i][1] for i in range(len(results))]
+b2 = [results[i][2] for i in range(len(results))]
+separations = [results[i][6] for i in range(len(results))]   
+kappas_e = [results[i][7] for i in range(len(results))]  
+kappas_i = [results[i][8] for i in range(len(results))]                                                              
+succs = [results[i][10] for i in range(len(results))]   
+decode_f = [results[i][11] for i in range(len(results))]  
+
+df=pd.DataFrame({'bias':final_biases, 'b1':b1, 'b2':b2, 'separation':separations, 'kappas_E':kappas_e,  'kappas_I':kappas_i, 'success':succs, 'decod_f':decode_f })
 
 
 
