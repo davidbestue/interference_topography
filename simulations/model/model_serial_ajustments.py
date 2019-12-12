d032309123c866b50ce0b0274000d819b0fcf4ed
davidbestue <davidsanchezbestue@hotmail.com>
2019-12-12 11:57:27 +0100

    Update model_serial_ajustments.py

--- simulations/model/model_serial_ajustments.py
+++ simulations/model/model_serial_ajustments.py
@@ -357,16 +357,29 @@ def model(totalTime, targ_onset, presentation_period, angle_separation, tauE=9, 
     final_bias = np.mean(final_bias)
 
     ## save each simulation
-    total_sim = 3
-    if save_each_!=False:
-        to_save = [ IE, WE, WI, rE, IEs, final_bias, bias_b1, bias_b2, rE, RE, estimated_angles, total_sep, kappa_E, kappa_I, r_squared, 
-        success, sigE, number_of_bumps, decode_func, std_g]
-        filename =  str(np.random.randint(total_sim)) + '.pkl'
-        while filename in os.listdir(path_save): #in case it has the same name, add a number behind
-                filename =  str(np.random.randint(total_sim)) + '.pkl'
+    too_many=100
+    os.chdir('C:\\Users\\David\\Desktop')
+    path_resp = os.path.join(os.getcwd(), 'simuls')  
+    total_sim = 10
+    save_each_ = True
 
+    import time
+    timeout = time.time() + 3   # 1 minutes from now
+    os.chdir(path_resp)
+
+    if save_each_!=False:
+        #to_save = [ IE, WE, WI, rE, IEs, final_bias, bias_b1, bias_b2, rE, RE, estimated_angles, total_sep, kappa_E, kappa_I, r_squared, 
+        #success, sigE, number_of_bumps, decode_func, std_g]
+        filename =  str(np.random.randint(total_sim)) + '.pkl'
+        while filename in os.listdir(path_resp): #in case it has the same name, add a number behind
+                filename =  str(np.random.randint(total_sim)) + '.pkl'
+                if time.time()>timeout:
+                    too_many+=1
+                    filename =  str(np.random.randint(total_sim)) + str(np.random.randint(too_many)) + '.pkl'
+        #
         with open(filename, 'wb') as fp:
             pickle.dump(to_save, fp)
+
 
 
 
@@ -473,3 +486,5 @@ if save_each_!=False:
 
 
 
+with open('66.pkl', 'rb') as f:
+    mynewlist = pickle.load(f)
