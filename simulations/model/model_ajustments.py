055eea37f63be00822f4ee41ac7f905824775a55
davidbestue <davidsanchezbestue@hotmail.com>
2019-12-27 09:00:11 +0100

    Update model_ajustments.py

--- simulations/model/model_ajustments.py
+++ simulations/model/model_ajustments.py
@@ -435,7 +435,6 @@ def model(totalTime, targ_onset, presentation_period, angle_separation, tauE=9, 
 #     kappas_i = kappas_i + [kappa_i_test[idx]]*rep_dist
 
 
-
 # results = Parallel(n_jobs = numcores)(delayed(model)(totalTime=2000, targ_onset=100,  presentation_period=350, angle_separation=22, tauE=9, tauI=4,  n_stims=1, I0E=0.1, I0I=0.5,
 #  GEE=0.025, GEI=0.019, GIE=0.01 , GII=0.1, sigE=1.1, sigI=1.9, kappa_E=kape, kappa_I=kapi, kappa_stim=75, N=512, plot_connectivity=False, plot_rate=False, plot_hm=False , plot_fit=False)  for kape, kapi in zip(kappas_e, kappas_i)) 
 
