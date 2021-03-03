87deaf214c93a9051075becd5078b3e6ad6a1260
David Bestue <davidsanchezbestue@hotmail.com>
2021-03-03 17:44:03 +0100

    update

--- simulations/model/radial/simulations_linear.py
+++ simulations/model/radial/simulations_linear.py
@@ -10,15 +10,6 @@ numcores = multiprocessing.cpu_count() - 3
 
 ############################################# delay 2 seconds ##############################################################################################################
 
-totalTime, targ_onset, presentation_period, position, 
-                         N=512, rint = 1, rext = 6,
-                         tauE=9, tauI=4,  
-                         I0E=0.1, I0I=0.5, 
-                         GEE=0.022, GEI=0.019, GIE=0.01 , GII=0.1, 
-                         NsigE=0.8, NsigI=1.7, 
-                         plot_connectivity=False, plot_rate=False, save_RE=False
-
-
 
 paths_save_= '/home/david/Desktop/IDIBAPS/Simulations_radial/results_simul_radial_linear.xlsx'
 
