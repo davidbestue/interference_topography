388b844d3e6d47a31c524318471e3f38ea8230ff
David Bestue <davidsanchezbestue@hotmail.com>
2021-05-24 10:44:03 +0200

    update

--- lab meeting examples/model_christos.py
+++ lab meeting examples/model_christos.py
@@ -278,7 +278,7 @@ def model(totalTime, targ_onset_1, targ_onset_2, presentation_period, angle_targ
         #### plot dynamics
         fig = plt.figure()
         plt.title('Rate dynamics')
-        plt.plot(RE[N-p_targ1, :], 'b', label='target1')
+        plt.plot(RE[N-p_targ1, :], 'b', label='target')
         plt.plot(RE[N-p_targ2, :], 'r', label='distractor')
         plt.xlabel('time (ms)')
         plt.ylabel('rate (Hz)')
@@ -287,7 +287,7 @@ def model(totalTime, targ_onset_1, targ_onset_2, presentation_period, angle_targ
         plt.gca().get_xaxis().tick_bottom()                                                                                         
         plt.gca().get_yaxis().tick_left()
         plt.gca().tick_params(direction='in') #direction
-        plt.legend(loc=4, frameon=False,  bbox_to_anchor=(1.25, 0.25), prop={'size': 14})
+        plt.legend(loc=4, frameon=False,  bbox_to_anchor=(1.4, 0.25), prop={'size': 14})
         plt.show(block=False)
     if plot_hm==True:
         #### plot heatmap
